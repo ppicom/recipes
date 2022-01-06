f98b0a8aa6a8dabf269f2658870784e7265b5070
Pere <perepicomuntaner@gmail.com>
2022-01-06 14:51:36 +0100

    Persist session in localstorage

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import {
   BehaviorSubject,
   catchError,
@@ -15,8 +16,9 @@ import User from './user.model';
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   user = new BehaviorSubject<User>(null);
+  session;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   signup(email: string, password: string) {
     return this.http
@@ -59,6 +61,8 @@ export class AuthService {
     const expiration = new Date(new Date().getTime() + +expiresIn * 1000);
     const user = new User(email, localId, idToken, expiration);
     this.user.next(user);
+    this.autoLogout(+expiresIn * 1000)
+    localStorage.setItem('user', JSON.stringify(user))
   }
 
   private handleAuthError(errorRes: HttpErrorResponse) {
@@ -84,6 +88,32 @@ export class AuthService {
 
   logout() {
     this.user.next(null);
+    this.router.navigate(['/auth'])
+    localStorage.removeItem('user')
+    if (this.session) {
+      clearTimeout(this.session)
+      this.session = null
+    }
+  }
+
+  autoLogout(expirationDuration: number) {
+    debugger;
+    this.session = setTimeout(() => {
+      this.logout()
+    }, expirationDuration);
+  }
+
+  autoLogin() {
+    const userString = localStorage.getItem('user')
+    if (userString) {
+      const flatUser = JSON.parse(userString)
+      const user = new User(flatUser.email, flatUser.ID, flatUser._token, new Date(flatUser._tokenExpiration))
+
+      if (user.token) {
+        this.user.next(user)
+        this.autoLogout(new Date(flatUser._tokenExpiration).getTime() - new Date().getTime())
+      }
+    }
   }
 
 }

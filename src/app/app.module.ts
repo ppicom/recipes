222b526158b23aec9b337b205db3cbbf3c04985f
Pere <perepicomuntaner@gmail.com>
2022-01-11 20:28:06 +0100

    chore: remove unused module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthInterceptor } from './auth/auth-interceptor.service';
-import { AuthComponent } from './auth/auth.component';
 import { AuthModule } from './auth/auth.module';
 import HeaderComponent from './header/header.component';
 import { RecipesModule } from './recipes/recipes.module';
@@ -22,7 +21,6 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     RecipesModule,
     ShoppingListModule,
